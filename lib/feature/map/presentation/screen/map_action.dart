6262614f4ffed1288772bafc290b907979f89806
Hagminism <iilhm110629@gmail.com>
2026-08-13 16:57:24 +0900

    feat: 지도 화면 내 카테고리 필터 및 현 위치 이동 액션 정의
    - `MapAction`에 `categoryFilterPressed` 정의를 추가하여 카테고리 선택 기능 대응
    - `MapAction`에 `currentLocationPressed` 정의를 추가하여 현재 위치 이동 기능 대응
    - `MapCategory` 모델 임포트 추가를 통한 관련 데이터 타입 지원

--- lib/feature/map/presentation/screen/map_action.dart
+++ lib/feature/map/presentation/screen/map_action.dart
@@ -1,5 +1,6 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:meomum/core/domain/model/location/geo_location.dart';
+import 'package:meomum/feature/map/presentation/model/map_category.dart';
 
 part 'map_action.freezed.dart';
 
@@ -8,4 +9,7 @@ sealed class MapAction with _$MapAction {
   const factory MapAction.mapReady(GeoLocation location) = MapReady;
   const factory MapAction.researchButtonPressed() = ResearchButtonPressed;
   const factory MapAction.searchBarPressed() = SearchBarPressed;
+  const factory MapAction.categoryFilterPressed(MapCategory category) =
+      CategoryFilterPressed;
+  const factory MapAction.currentLocationPressed() = CurrentLocationPressed;
 }
