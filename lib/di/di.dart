f8f1c15def9cfbdf71602158c0fb081fb02f6747
Hagminism <iilhm110629@gmail.com>
2026-08-20 18:48:50 +0900

    feat: http 패키지 의존성 추가 및 httpClientProvider 구현
    - `http` 패키지 임포트 및 `http.Client` 인스턴스를 제공하는 `httpClientProvider` 추가

--- lib/di/di.dart
+++ lib/di/di.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:http/http.dart' as http;
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 final supabaseClientProvider = Provider<SupabaseClient>((Ref ref) {
@@ -15,3 +16,6 @@ final geolocatorProvider = Provider<GeolocatorPlatform>((Ref ref) {
   return GeolocatorPlatform.instance;
 });
 
+final httpClientProvider = Provider<http.Client>((Ref ref) {
+  return http.Client();
+});
