29f0fe7a90dce991f16eaf8325d5b89e5fa12547
Hagminism <iilhm110629@gmail.com>
2026-08-25 23:58:42 +0900

    refactor: DI 모듈에서 GoogleSignIn 관련 코드 제거
    - 사용하지 않는 `googleSignInProvider` 삭제
    - 불필요한 `google_sign_in` 패키지 임포트 제거

--- lib/di/di.dart
+++ lib/di/di.dart
@@ -1,15 +1,10 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:http/http.dart' as http;
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 final supabaseClientProvider = Provider<SupabaseClient>((Ref ref) {
   return Supabase.instance.client;
-});
-
-final googleSignInProvider = Provider<GoogleSignIn>((Ref ref) {
-  return GoogleSignIn.instance;
 });
 
 final geolocatorProvider = Provider<GeolocatorPlatform>((Ref ref) {
