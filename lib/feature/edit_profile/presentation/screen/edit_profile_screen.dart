7096a1c5f7940150225d21cf5d988786a36fd046
Hagminism <iilhm110629@gmail.com>
2026-09-09 15:10:05 +0900

    feat: 프로필 편집 화면 구현
    - 프로필 편집 액션, 상태, 이벤트, ViewModel 추가
    
    - 이미지 선택과 프로필 저장 결과 연결

---
+++ lib/feature/edit_profile/presentation/screen/edit_profile_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:meomum/feature/edit_profile/presentation/screen/edit_profile_action.dart';
+import 'package:meomum/feature/edit_profile/presentation/screen/edit_profile_state.dart';
+import 'package:meomum/core/presentation/component/profile_editor.dart';
+import 'package:meomum/ui/app_colors.dart';
+
+class EditProfileScreen extends StatelessWidget {
+  final EditProfileState state;
+  final void Function(EditProfileAction) onAction;
+
+  const EditProfileScreen({
+    super.key,
+    required this.state,
+    required this.onAction,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle.dark,
+      child: Scaffold(
+        backgroundColor: AppColors.homeBackground,
+        body: SafeArea(
+          child: ProfileEditor(
+            title: '프로필을 수정해보세요',
+            description:
+                '커뮤니티에서 사용할 프로필을 수정해보세요.\n'
+                '프로필은 커뮤니티에서 다른 사람에게 보여요.',
+            nickname: state.nickname,
+            avatarUrl: state.avatarUrl,
+            selectedImagePath: state.selectedImagePath,
+            isLoading: state.isLoading,
+            isValid: state.isValid,
+            submitLabel: '변경사항을 저장할게요',
+            onAvatarTap: () {
+              onAction(const EditProfileAction.tapAvatar());
+            },
+            onNicknameChanged: (String nickname) {
+              onAction(EditProfileAction.changeNickname(nickname));
+            },
+            onBack: () {
+              onAction(const EditProfileAction.tapBack());
+            },
+            onSubmit: () {
+              onAction(const EditProfileAction.tapSubmit());
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
